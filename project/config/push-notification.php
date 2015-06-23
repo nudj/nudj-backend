ff64ce20e7eaab0d66f450fa368dab262b9b8779
iivannov <ivan@e-man.co.uk>
2015-06-23 19:06:24 +0300

    user base_path

--- project/config/push-notification.php
+++ project/config/push-notification.php
@@ -3,7 +3,7 @@
 return [
     'NudgeIOS'    =>  [
         'environment' => 'development',
-        'certificate' => app_path('resources/certificates/ck.pem'),
+        'certificate' => base_path('resources/certificates/ck.pem'),
         'passPhrase'  => 'nudj2015',
         'service'     => 'apns'
     ]
