421ae88bbdc34c9eaf41f7e5ecf77411002eb30d
mladenovs@gmail.com <mladenovs@gmail.com>
2015-06-24 15:41:02 +0300

    some stuff ... continue with the css

--- project/app/Http/Controllers/Web/WebController.php
+++ project/app/Http/Controllers/Web/WebController.php
@@ -36,6 +36,22 @@ class WebController extends \Illuminate\Routing\Controller
         $data = [
             'user' => [
                 'name' => 'Simo'
+            ],
+            'job'  => (object) [
+                'title' => 'UX Designer (Permanent)',
+                'from'  => 'Jeremy Garza',
+                'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+                'tags' => [
+                    'Photoshop','Corel Draw'
+                ],
+                'employer' => 'Digitalz',
+                'location' =>(object)[
+                    'name' => 'London',
+                    'lat'  => '51.508742',
+                    'lon'  => '-0.11055'
+                ],
+                'salary' => '25K - 35K',
+                'status' => 1
             ]
         ];
         return view('web/page/job',$data);
--- project/resources/views/web/page/job.blade.php
+++ project/resources/views/web/page/job.blade.php
@@ -21,22 +21,46 @@
         </div>
     </nav>
 
-    <div id="name" class="container">
+    <div id="name" class="container-fluid no-padding">
         <div class="col-xs-12 col-centered col-max">
-            <p class="lead newp">
-
-            </p>
-
+            <div id="job-title" class="container-fluid">
+                {{$job->title}}
+            </div>
+            <div id="job-from" class="container-fluid">
+                Posted from: {{$job->from}}
+            </div>
+            <div id="job-description">
+                {{$job->description}}
+            </div>
+            <div id="job-tags" class="container-fluid">
+                @foreach ($job->tags as $tag)
+                    <div>{{ $tag }}</div>
+                @endforeach
+            </div>
+            <div id="job-employer" class="container-fluid">
+                <img class="status-check" src="{{ asset('assets/web/img/employer.png') }}"/> Employer: {{$job->employer}}
+            </div>
+            <div id="job-location" class="container-fluid">
+                        <div><img class="status-check" src="{{ asset('assets/web/img/pin.png') }}"/> Location: {{$job->location->name}}</div>
+                        <div data-lat ="{{$job->location->lat}}" data-lon="{{$job->location->lon}}">View map</div>
+            </div>
+            <div id="job-salary" class="container-fluid">
+                <img class="status-check" src="{{ asset('assets/web/img/salary.png') }}"/> Salary: &pound; {{$job->salary}}
+            </div>
         </div>
     </div>
 
     <div id="status" class="container-fluid no-padding">
         <div class="in-status">
             <div id="left-cell">
-                Job Status
+                <img class="status-check" src="{{ asset('assets/web/img/suitcase.png') }}"/>Job Status
             </div>
             <div id="right-cell">
-                <img class="status-check" src="{{ asset('assets/web/img/check.png') }}"/>Active
+                @if ($job->status === 1)
+                <img class="status-check" src="{{ asset('assets/web/img/check.png') }}"/><span style="color: #00A187;">Active</span>
+                @else
+                    <span>Inactive</span>
+                @endif
             </div>
         </div>
     </div>
