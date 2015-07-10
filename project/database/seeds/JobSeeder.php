72e819a9eb66a204d1642e3caf4f4a5b42498f16
iivannov <ivan@e-man.co.uk>
2015-07-10 15:46:38 +0300

    final test

--- project/database/seeds/JobSeeder.php
+++ project/database/seeds/JobSeeder.php
@@ -14,6 +14,32 @@ class JobSeeder extends Seeder {
 		DB::table('nudges')->truncate();
 		DB::table('jobs')->truncate();
 
+
+		DB::table('jobs')->insert([
+			'id' => 1,
+			'user_id' => 3,
+			'title' => 'Junior web developer',
+			'location' => 'Sofia',
+			'bonus' => 300,
+			'company' => 'DevEmployer ltd',
+			'salary' => 'Circa 25k - 30k',
+			'description' =>  "We aren't looking for an ambitious junior web developer to join our team",
+			'active' => 1,
+		]);
+
+		DB::table('jobs')->insert([
+			'id' => 2,
+			'user_id' => 4,
+			'title' => 'Architect',
+			'location' => 'Hoxton, London',
+			'bonus' => 225,
+			'company' => 'OurArchitectureCompany Ltd',
+			'salary' => '50 000',
+			'description' =>  "OurArchitectureCompany Ltd. is looking for Part-II or Part-III architects to join our practice in Hoxton, London.The studio is a growing practice and has a fun, creative culture with exciting design focused work delivered by a close knit team of designers.",
+			'active' => 1,
+		]);
+
+
 		$this->command->info('Jobs seeded!');
 
 	}
--- project/database/seeds/UserSeeder.php
+++ project/database/seeds/UserSeeder.php
@@ -19,6 +19,38 @@ class UserSeeder extends Seeder {
 			'roles' => json_encode(['admin'])
 		]);
 
+		DB::table('users')->insert([
+			'id' => 2,
+			'token' => 'CryUOlx7AN8k3PKQxJVh0JmH7ETiK6xUj8uSpZIn61dEZGZFhMSbyaAQyVks',
+			'phone' => '+44123456789',
+			'country_code' => 'GB',
+			'verification' => 1111,
+			'verified' => 1,
+			'status' => 1,
+			'mobile' => 1,
+			'name' => 'Vanessa',
+			'image' => '{"profile":"XQrFsvPlbz.png","cover":"XQrFsvPlbz.jpg"}',
+			'notifications' => '{"notifications":{"1":true,"2":true,"3":true,"4":true,"5":true}}'
+		]);
+
+		DB::table('users')->insert([
+			'id' => 3,
+			'token' => 'CryUOlx7AN8k3PKQxJVh0JmH7ETiK6xUj8uSpZIn61dEZGZFhMSbyaAQyVks',
+			'phone' => '+359884676575',
+			'country_code' => 'GB',
+			'verification' => 1111,
+			'verified' => 1,
+			'status' => 1,
+			'mobile' => 1,
+			'name' => 'Ivan',
+			'address' => 'London',
+			'position' => 'Developer',
+			'company' => 'E-man',
+			'about' => 'Everything is possible. The impossible just takes me more timet. Test',
+			'image' => '{"profile":"AHf92G5rK3.png","cover":"AHf92G5rK3.jpg"}',
+			'notifications' => '{"notifications":{"1":true,"2":true,"3":true,"4":true,"5":true}}'
+		]);
+
 		$this->command->info('Users seeded!');
 
 
