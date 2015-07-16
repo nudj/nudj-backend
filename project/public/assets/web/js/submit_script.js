436206744c2409d875110491f66bbfea88619e4d
mladenovs@gmail.com <mladenovs@gmail.com>
2015-07-16 19:37:35 +0300

    uncomment

--- project/public/assets/web/js/submit_script.js
+++ project/public/assets/web/js/submit_script.js
@@ -27,24 +27,24 @@ $("#btn-submit").click(function(e){
         var put_data = {phone:phone,verification:verific,country_code:country_code};
         $.post( base_path +"/verify", put_data,function(data) {})
             .done(function( data ) {
-                /*console.log(data.success);*/
-                //isVerifyet = JSON.stringify(data);
-                //var obj_verifyet = eval('('+isVerifyet+')');
-                //if(data.success){
-                //   /* console.log("result"+obj_verifyet);*/
-                //    if($("#reqfrom").val() == "refer")
-                //         window.location.href = base_path +"/job/"+$("#jobid").val();
-                //    else
-                //        window.location.href = base_path +"/job/"+$("#jobid").val();
-                //}
-                //else{
-                //    TINY.box.show({html:msgFail,width:200,height:200,fixed:false,maskid:'bluemask',maskopacity:40,close:false,closejs:function(){closeFailed()}})
-                //}
-                window.location.href = base_path +"/job/"+$("#jobid").val();
+                console.log(data.success);
+                isVerifyet = JSON.stringify(data);
+                var obj_verifyet = eval('('+isVerifyet+')');
+                if(data.success){
+                   /* console.log("result"+obj_verifyet);*/
+                    if($("#reqfrom").val() == "refer")
+                         window.location.href = base_path +"/job/"+$("#jobid").val();
+                    else
+                        window.location.href = base_path +"/job/"+$("#jobid").val();
+                }
+                else{
+                    TINY.box.show({html:msgFail,width:200,height:200,fixed:false,maskid:'bluemask',maskopacity:40,close:false,closejs:function(){closeFailed()}})
+                }
+                //window.location.href = base_path +"/job/"+$("#jobid").val();
             })
             .fail(function(){
-                //TINY.box.show({html:msgFail,width:200,height:200,fixed:false,maskid:'bluemask',maskopacity:40,close:false,closejs:function(){closeFailed()}})
-                window.location.href = base_path +"/job/"+$("#jobid").val();
+                TINY.box.show({html:msgFail,width:200,height:200,fixed:false,maskid:'bluemask',maskopacity:40,close:false,closejs:function(){closeFailed()}})
+                //window.location.href = base_path +"/job/"+$("#jobid").val();
             })
     }
 
