609cd94bff9e3c07ed03cfac4a53cbb65380221e
iivannov <ivan@e-man.co.uk>
2015-07-23 16:27:41 +0300

    test mongo

--- project/app/Http/Controllers/ServicesController.php
+++ project/app/Http/Controllers/ServicesController.php
@@ -5,6 +5,7 @@ use App\Models\Skill;
 use App\Utility\ApiException;
 use App\Utility\ApiExceptionType;
 use App\Utility\Facades\Shield;
+use App\Utility\Logger\Log;
 use App\Utility\Util;
 use Elasticsearch\Client;
 use Illuminate\Support\Facades\Config;
@@ -24,9 +25,11 @@ class ServicesController extends ApiController
 
     public function test()
     {
-
-
+        $log = new Log();
+        $log->date = time();
+        $log->save();
         $logs = DB::connection('mongodb')->collection('logs')->get();
+
         print_r($logs);
     }
 
