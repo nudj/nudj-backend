3528ee613326c2756026a7a2b1ae9677f865be43
iivannov <ivan@e-man.co.uk>
2015-07-23 18:04:27 +0300

    echo

--- project/app/Http/Controllers/ServicesController.php
+++ project/app/Http/Controllers/ServicesController.php
@@ -1,15 +1,10 @@
 <?php namespace App\Http\Controllers;
 
-use App\Models\Job;
-use App\Models\Skill;
+
 use App\Utility\ApiException;
 use App\Utility\ApiExceptionType;
 use App\Utility\Facades\Shield;
 use App\Utility\Logger\Log;
-use App\Utility\Util;
-use Elasticsearch\Client;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
 
 class ServicesController extends ApiController
 {
@@ -25,12 +20,14 @@ class ServicesController extends ApiController
 
     public function test()
     {
-//        $logs = DB::connection('mongodb')->collection('logs')->get();
+
 
         $logs = Log::all();
 
         foreach($logs as $log)
-            $log->display();
+           echo $log->display();
+
+
     }
 
 
