f6a90bf8496147a6a1e937ae0f915d9e0d97bb9a
Lachezar Todorov <realbudhead@gmail.com>
2015-08-11 16:00:05 +0300

    test

--- project/app/Handlers/Events/StartChat.php
+++ project/app/Handlers/Events/StartChat.php
@@ -11,6 +11,8 @@ use Fabiang\Xmpp\Protocol\Presence;
 use GameNet\Jabber\RpcClient;
 use Illuminate\Contracts\Queue\ShouldBeQueued;
 
+use Log;
+
 use Illuminate\Support\Facades\Config;
 
 
@@ -23,7 +25,9 @@ class StartChat implements ShouldBeQueued
 
         $initiator = User::findOrFail($event->initiatorId);
         $initiatorUsername = $initiator->id . '@chat.nudj.co';
-        $interlocutorUsername = $event->interlocutorId . '@chat.nudj.co';
+        $interlocutorUsername = $event->interlocutorId . '@chat.nudj.co'; // WTF! (lacho)
+
+	    Log::info('Chat Creation started: '. $initiatorUsername . ' :: ' . $interlocutorUsername);
 
         // Create room and invite people
         $rpc = new RpcClient([
@@ -32,13 +36,20 @@ class StartChat implements ShouldBeQueued
             'debug' => false,
         ]);
 
-        $rpc->createRoom((string) $event->chatId);
-        $rpc->inviteToRoom($event->chatId, null, null, [$initiatorUsername, $interlocutorUsername]);
+	    Log::info('RPC connection to the server established');
+
+        $createRoomStatus = $rpc->createRoom((string) $event->chatId);
+
+	    Log::info('Room Created: ' . $createRoomStatus);
+
+        $inviteStatus = $rpc->inviteToRoom($event->chatId, null, null, [$initiatorUsername, $interlocutorUsername]);
+	    Log::info('Invite Created: ' . $inviteStatus);
 
         // Connect trough XMPP
         $options = new Options(Config::get('cfg.chat_server_tcp'));
         $options->setUsername($initiator->id)
-            ->setPassword($initiator->token);
+            ->setPassword($initiator->token)
+            ->setLogger(Log::getMonolog());
 
         $client = new Client($options);
         $client->connect();
@@ -46,10 +57,14 @@ class StartChat implements ShouldBeQueued
         // Join the room
         $roomFullName = $event->chatId . Config::get('cfg.chat_conference_domain');
 
+	    Log::info('RPC connection to the server established');
+
         $channel = new Presence;
         $channel->setTo($roomFullName)
             ->setNickName($initiatorUsername);
         $client->send($channel);
+
+	    Log::info('Joined the room: ' . $roomFullName);
 
         // Write your message
         $message = new Message;
@@ -57,6 +72,8 @@ class StartChat implements ShouldBeQueued
             ->setTo($roomFullName)
             ->setType(Message::TYPE_GROUPCHAT);
         $client->send($message);
+
+	    Log::info('Sent Message: ' . $message);
 
         // Bye bye
         $client->disconnect();
