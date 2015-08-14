df126d6b06be715b3ded40bcd911845aa2d369bc
Lachezar Todorov <realbudhead@gmail.com>
2015-08-14 12:11:24 +0300

    notification wording

--- project/resources/lang/en/notifications.php
+++ project/resources/lang/en/notifications.php
@@ -6,10 +6,10 @@ return [
 
     // General Errors
     NotificationType::$ASK_TO_REFER => ':employer : :message',
-    NotificationType::$APP_APPLICATION => ':candidate referred by :referrer is interested for the :position position',
-    NotificationType::$WEB_APPLICATION => ':candidate referred by :referrer is interested for the :position position',
+    NotificationType::$APP_APPLICATION => ':candidate referred by :referrer is interested in the :position position',
+    NotificationType::$WEB_APPLICATION => ':candidate referred by :referrer is interested in the :position position',
     NotificationType::$MATCHING_CONTACT => ':candidate might be a good match for the position of :job, posted by :employer',
-    NotificationType::$APP_APPLICATION_NOREF => ':candidate is interested for the :position position',
-    NotificationType::$WEB_APPLICATION_NOREF => ':candidate is interested for the :position position',
+    NotificationType::$APP_APPLICATION_NOREF => ':candidate is interested in the :position position',
+    NotificationType::$WEB_APPLICATION_NOREF => ':candidate is interested in the :position position',
 
 ];
