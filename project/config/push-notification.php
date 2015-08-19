b84e1953dc01e0947e270e8d644dfcf1fe59607d
iivannov <ivan@e-man.co.uk>
2015-08-19 17:55:12 +0300

    use the development certificate

--- project/config/push-notification.php
+++ project/config/push-notification.php
@@ -3,7 +3,7 @@
 return [
     'NudgeIOS' => [
         'environment' => 'production',
-        'certificate' => base_path('resources/certificates/production.pem'),
+        'certificate' => base_path('resources/certificates/development.pem'),
         'passPhrase' => 'power123',
         'service' => 'apns'
     ]
