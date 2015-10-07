00bf2397911225d7a3a01122f49c570f117dd6ca
iivannov <ivan@e-man.co.uk>
2015-10-07 16:12:17 +0300

    ADD: admin molde L5 compatible

---
+++ project/app/Models/Admin.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Contracts\HasRolesContract;
+use App\Models\Traits\HasRoles;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class Admin extends Model implements AuthenticatableContract, CanResetPasswordContract, HasRolesContract
+{
+    use Authenticatable, CanResetPassword, HasRoles;
+
+
+
+    protected $table = 'admins';
+
+    protected $fillable = ['name', 'email', 'password', 'roles'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+
+    public static function add($request){
+
+          $admin = new Admin();
+
+          $admin->name = (string)$request->name;
+          $admin->email = (string)$request->email;
+          $admin->password = (string)Hash::make($request->password);
+          $admin->roles = "['admin']";
+
+
+        $admin->save();
+
+        return $admin;
+    }
+
+
+
+    public function edit($data)
+    {
+
+        if (isset($data['name']))
+            $this->name = (string)$data['name'];
+
+        if (isset($data['email']))
+            $this->email = (string)$data['email'];
+
+        if (isset($data['password']))
+            $this->password = (string)Hash::make($data['password']);
+
+        $this->save();
+
+        return true;
+    }
+
+}
