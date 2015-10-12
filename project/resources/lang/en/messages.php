4007ac73fa231a5cc503f19c7e2bff5f9c5e90ad
iivannov <ivan@e-man.co.uk>
2015-10-12 15:24:06 +0300

    CHANGE: SMS/Notifications update

--- project/resources/lang/en/messages.php
+++ project/resources/lang/en/messages.php
@@ -2,7 +2,9 @@
 
 return [
 
-    'nudge' => 'Please check this job offer',
-    'refer' => 'Can you refer someone for the position of :position',
+    'nudge_sms' => "I was asked if I know anyone good for this job and thought of you :-)",
+    'nudge_chat' => "I was asked if I know anyone good for this job and thought of you :-) Take a look",
+    'refer' => "Hey, I'm hiring! Who would you recommend for this?",
+
 
 ];
