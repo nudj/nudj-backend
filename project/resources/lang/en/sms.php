2b999ef3432961e6eeed3777ba452365db6a084a
iivannov <ivan@e-man.co.uk>
2015-10-16 14:36:41 +0300

    Changes in sms messsages

--- project/resources/lang/en/sms.php
+++ project/resources/lang/en/sms.php
@@ -3,8 +3,8 @@
 return [
 
     'verification' => "Your Nudj verification code is :code",
-    'refer' => ":name has messaged you: \n :message \n More job info here: :link",
-    'nudge' => ":message \n Take a look: :link",
-    'invite' => "Hey! :name wants you to check out nudj. The app lets you hire, refer and even apply for jobs in a couple of clicks. Take a look! \n :link",
+    'refer' => ":name has messaged you via Nudj: \n :message \n More job info here: :link",
+    'nudge' => ":name has messaged you via Nudj: \n :message \n Take a look: :link",
+    'invite' => "Hey! :name wants you to check out Nudj. The app lets you hire, refer and even apply for jobs in a couple of clicks. Take a look! \n :link",
 
 ];
