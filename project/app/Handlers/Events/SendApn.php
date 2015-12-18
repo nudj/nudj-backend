c30777f210c41bb5eddd8c222a67dc3e1ce48876
Pascal Honoré <pascal@alseyn.net>
2015-12-18 15:43:15 +0000

    Note on NotifyUserEvent

--- project/app/Handlers/Events/SendApn.php
+++ project/app/Handlers/Events/SendApn.php
@@ -16,6 +16,24 @@ class SendApn implements ShouldBeQueued
     {
 
         $devices = User::min()->find($event->recipientId)->devices()->get();
+        /*
+
+            NOTE: Was wondering where what appears to be this "min" static function was defined
+
+            It's a conjuction of few things
+                1. The inheritance chain from Users is Users <- ApiModel <- Model
+                2. None of them implements "min"
+                3. Actually Model has a __call function which in this case (method name is not 'increment' or 'decrement') forwards the call to an instance of Builder.
+                    see: http://php.net/manual/en/language.oop5.magic.php
+                4. Builder implements __call and in this instance [*] calls scopeMin which is defined in ApiModel.
+
+                [*] the method name starts with "scope"
+                    see: http://laravel.com/docs/5.1/eloquent#query-scopes
+
+            NOTE: User::min() and similar expression for childrens of ApiModel
+                simply selects by the primary key.
+
+        */
         $notificationCount = Notification::getNewNotificationsCount($event->recipientId);
 
         $options['badge'] = $notificationCount;
