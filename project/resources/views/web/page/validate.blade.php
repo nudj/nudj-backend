693bbf7ccc2393aaa55ed005574a106561f2fd60
Pascal Honoré <pascal@alseyn.net>
2016-01-10 21:03:50 +0000

    Fixing display on the Android phone (temporary fix)

--- project/resources/views/web/page/validate.blade.php
+++ project/resources/views/web/page/validate.blade.php
@@ -11,7 +11,7 @@
 
 @section('page')
 
-	<!-- 
+    <!-- 
     <nav class="navbar navbar-inverse navbar-fixed-top coloredhead">
         <div class="container">
             <img class="logo" src="{{ asset('assets/web/img/nudj_logo.png') }}"/>
