a40e613a786278c2bad81cd753c5923df02a3bc8
Pascal Honoré <pascal@alseyn.net>
2016-01-16 17:50:49 +0000

    Testing: GET api/v1/users/{userid}

--- nudj-api/tests/APIUsersTest.php
+++ nudj-api/tests/APIUsersTest.php
@@ -70,4 +70,33 @@ class APIUsersTest extends TestCase {
 		$this->assertArrayHasKey('name', $xp1['data'][0]);
 		$this->assertArrayHasKey('pagination', $xp1);
 	}
+	public function test2()
+	{
+		$uri = 'api/v1/users/1';
+		$method = 'GET';
+		$parameters = [];
+		$cookies = [];
+		$files = [];
+		$server = [];
+		$content = null;
+		$request = Request::create($uri, $method, $parameters, $cookies, $files, $server, $content);
+		$request->headers->set('token','JD7duPsAC1qgea4UD4otZpBG2wLKBxFIIhz32zFk1RdwWR4bsiCjeFwofWSz');
+		$response = $this->app->make('Illuminate\Contracts\Http\Kernel')->handle($request);
+
+		$this->assertEquals(200, $response->getStatusCode());
+		$xp1 = json_decode($response->getContent(),true);
+		$this->assertInternalType('array', $xp1);
+		/*
+			{
+				"data": {
+					"id": "1",
+					"name": "Lachezar Todorov"
+				},
+				"timestamp": 1452451924.4747
+			}
+		*/
+		$this->assertArrayHasKey('data', $xp1);
+		$this->assertArrayHasKey('id', $xp1['data']);
+		$this->assertArrayHasKey('name', $xp1['data']);
+	}
 }
