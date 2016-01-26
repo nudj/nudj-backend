859b9a5f02a94df4cfacf892d13aece4df23f879
Pascal Honoré <pascal@alseyn.net>
2016-01-26 12:33:39 +0000

    update in config/push-notification.php

--- nudj-api/config/push-notification.php
+++ nudj-api/config/push-notification.php
@@ -4,8 +4,8 @@ return [
     'NudgeIOS' => [
         'environment' => 'production',
         'certificate' => base_path('resources/certificates/production.pem'),
-        'passPhrase' => 'power123',
-        'service' => 'apns'
+        'passPhrase'  => '',
+        'service'     => 'apns'
     ]
 ];
 
