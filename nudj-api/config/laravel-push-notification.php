1ee5f71324b80a28c0807d03a0ac03593833f483
Pascal Honoré <pascal@alseyn.net>
2016-02-16 18:38:20 +0000

    Adding missing congif file (filename from third party source code)

---
+++ nudj-api/config/laravel-push-notification.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'NudgeIOS' => [
+        'environment' => 'production',
+        'certificate' => base_path('resources/certificates/production.pem'),
+        'passPhrase' => 'turned4437.handwritings',
+        'service' => 'apns'
+    ]
+];
+
