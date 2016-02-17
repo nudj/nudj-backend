c584ab6cacc440ce5faa29a54a2ddd992e7d42d0
Pascal Honoré <pascal@alseyn.net>
2016-02-17 15:57:31 +0000

    rebuilding SendApn

--- nudj-api/app/Handlers/Events/SendApn.php
+++ nudj-api/app/Handlers/Events/SendApn.php
@@ -13,40 +13,52 @@ class SendApn
     public function handle(NotifyUserEvent $event)
     {
 
+        // ------------------------------------------------
+        // Instanciating a PushManager
+
+        $pushManager = new PushManager(PushManager::ENVIRONMENT_DEV);
+
+        // ------------------------------------------------
+        // Building the DeviceCollection
+
         $devices = User::min()->find($event->recipientId)->devices()->get();
-        /*
+        $arrayOfDs = array();
+        foreach($devices as $device){
+            $arrayOfDs[] = new Device($device->token);
+        }
+        $devices = new DeviceCollection($arrayOfDs);
 
-            NOTE: Was wondering where what appears to be this "min" static function was defined
 
-            It's a conjuction of few things
-                1. The inheritance chain from Users is Users <- ApiModel <- Model
-                2. None of them implements "min"
-                3. Actually Model has a __call function which in this case (method name is not 'increment' or 'decrement') forwards the call to an instance of Builder.
-                    see: http://php.net/manual/en/language.oop5.magic.php
-                4. Builder implements __call and in this instance [*] calls scopeMin which is defined in ApiModel.
+        // ------------------------------------------------
+        // Setting up the APNS Adapter
 
-                [*] the method name starts with "scope"
-                    see: http://laravel.com/docs/5.1/eloquent#query-scopes
+        $apnsAdapter = new ApnsAdapter(array(
+            'certificate' => base_path('resources/certificates/production.pem'),
+            'passPhrase' => 'turned4437.handwritings'
+        ));
 
-            NOTE: User::min() and similar expression for childrens of ApiModel
-                simply selects by the primary key.
+        // ------------------------------------------------
+        // Making the Notification Options
 
-        */
-        $notificationCount = Notification::getNewNotificationsCount($event->recipientId);
-
-        $options['badge'] = $notificationCount;
+        $notification_options = array();
+        $notification_options['badge'] = Notification::getNewNotificationsCount($event->recipientId);
+        $notification_options['sound'] = 'default';
         if($event->meta) {
-            $options['custom'] = ['meta' => $event->meta];
+            $notification_options['custom'] = ['meta' => $event->meta];
         }
 
-        foreach($devices as $device) {
+        // ------------------------------------------------
+        // Building the message objects
 
-            $notifier = new PushNotification();
-            $notifier->app('NudgeIOS')
-                ->to($device->token)
-                ->send($event->message, $options);
+        $message = new Message('This is a test notification from Nudj', $notification_options);
 
-        }
+        // ------------------------------------------------
+        // Pushing
+
+        $push = new Push($apnsAdapter, $devices, $message);
+        $pushManager->add($push);
+        $pushManager->push();
+
     }
 
 }
