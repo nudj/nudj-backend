cb4f804abecba60bb5c48f5c3a1df35684605545
Pascal Honoré <pascal@alseyn.net>
2016-08-20 17:20:00 +0100

    -

--- nudj-web-application/app/Http/routes.php
+++ nudj-web-application/app/Http/routes.php
@@ -17,13 +17,9 @@ Route::group(['prefix' => '/'], function () {
 	// Relevant Information: 2c4e861f-be97-49a2-ad53-2d9c2f6a7f74
 
     Route::get('register/{jobId}', 'Web\WebController@login2');
-
     Route::post('validate', 'Web\WebController@validate2'); # The page after submitting your phone number and where you need to put the pin 
-
     Route::post('verify', 'Web\ActionsController@verify'); # This is a AJAX call to validate the 4 letter digit validation code. 
-
     Route::get('job/{jobId}/{hash?}', 'Web\WebController@job');
-
     Route::post('apply', 'Web\ActionsController@apply');
     Route::post('refer', 'Web\ActionsController@nudge');
     Route::get('countries', 'Web\ActionsController@countries');
@@ -36,7 +32,6 @@ Route::group(['prefix' => '/'], function () {
     Route::get('applying/{jobId}', 'Web\WebController@applying');    
     Route::get('appdownloads', 'Web\WebController@appdownloads'); 
     Route::get('nudj-a-friend/{jobId}', 'Web\WebController@nudjAFriend'); 
-
 
 });
 
