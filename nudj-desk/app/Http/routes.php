6412581ac9c0c8cb3f80c109024dff639ac79b83
Pascal Honoré <pascal@alseyn.net>
2016-08-21 18:32:08 +0100

    Removing un-used code

--- nudj-desk/app/Http/routes.php
+++ nudj-desk/app/Http/routes.php
@@ -74,10 +74,3 @@ Route::group(['prefix' => 'deskapi'], function () {
 
 });
 
-// -------------------------------------------------------------------------------
-// Listen for some stuff
-if (env('APP_ENV') != 'production' && Input::get('debug') == 'sql') {
-    Event::listen('illuminate.query', function ($query) {
-        echo($query . "\r\n");
-    });
-}
