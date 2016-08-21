0d73f6ef0f1647b8debdc0ec7c2d282661e9dd6f
Pascal Honoré <pascal@alseyn.net>
2016-08-21 18:54:01 +0100

    Update in navigation display

--- nudj-desk/resources/views/desk/partials/navigation.blade.php
+++ nudj-desk/resources/views/desk/partials/navigation.blade.php
@@ -2,21 +2,11 @@
     <!-- <li class="header">MAIN NAVIGATION</li> -->
     <li><a href="{{ url('dashboard') }}"><span>Dashboard</span></a></li>
     <li><a href="{{ url('users') }}">Users</a></li>
-    <li>
-        <a href="#"><span>Jobs</span></a>
-        <ul class="">
-            <li><a href="{{ url('jobs') }}">Listing</a></li>
-            <li><a href="{{ url('jobs/create') }}">Create New Job</a></li>
-        </ul>
-    </li>
+    <li><a href="{{ url('jobs') }}">List Job</a></li>
+    <li><a href="{{ url('jobs/create') }}">Create New Job</a></li>
     <li><a href="{{ url('staticcontents') }}">Static Contents</a></li>
     <li><a href="{{ url('jobapplications') }}">Job Applications</a></li>
-    <li>
-        <a href="#"><span>Admins</span></a>
-        <ul class="">
-            <li><a href="{{ url('admins') }}">View</a></li>
-            <li><a href="{{ url('admins/create') }}">Create</a></li>
-        </ul>
-    </li>
+    <li><a href="{{ url('admins') }}">List Admins</a></li>
+    <li><a href="{{ url('admins/create') }}">Create Admin</a></li>
     <li><a href="{{ url('auth/logout') }}"><span>Log Out</span></a></li>
 </ul>
